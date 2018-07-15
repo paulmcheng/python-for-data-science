afb412e646c2fdce7b02454d9ecde83288f5a78f
Paul <33575597+paulmcheng@users.noreply.github.com>
2018-07-15 21:11:56 +0100

    learn functions

---
+++ Chapter3-1-Functions/InbuiltFunctions.py
@@ -0,0 +1,18 @@
+# Create variables var1 and var2
+var1 = [1, 2, 3, 4]
+var2 = True
+
+# Print out type of var1
+print(type(var1))
+
+# Print out length of var1
+print(len(var1))
+
+# Convert var2 to an integer: out2
+out2 = int(var2)
+print(out2)
+
+# Help! fucntion
+#?sorted
+#You'll see that sorted() takes three arguments: iterable, key and reverse.
+#key=None means that if you don't specify the key argument, it will be None. reverse=False means that if you don't specify the reverse argument, it will be False.
